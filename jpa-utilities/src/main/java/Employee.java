8dc8af588d0300c340be2fad1c4d810e45fbb813
Dhrub <dhrubajyoti.biswas2@transport.nsw.gov.au>
2019-06-21 00:25:19 +1000

    Initial Commit

---
+++ jpa-utilities/src/main/java/Employee.java
@@ -0,0 +1,13 @@
+import javax.persistence.Entity;
+
+@Entity
+public class Employee {
+
+    private Long id;
+
+    private String name;
+    private String address;
+    private String email;
+
+
+}
---
+++ jpa-utilities/src/test/java/com/hcl/academy/Test.java
@@ -0,0 +1,10 @@
+package com.hcl.academy;
+
+
+class Test {
+
+
+    void getHello() {
+
+    }
+}
